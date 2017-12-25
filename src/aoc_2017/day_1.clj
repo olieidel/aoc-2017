a3d47b0562b970a1dd9aa2b47faa7ed53b129cba
Oliver Eidel <oli@eidel.net>
2017-12-25 16:26:17 +0100

    Actually complete day 1.

--- src/aoc_2017/day_1.clj
+++ src/aoc_2017/day_1.clj
@@ -9,7 +9,7 @@
        seq
        (map #(Character/digit % 10))))
 
-(defn compute [input]
+(defn part-1 [input]
   (loop [xs input
          prior (last input)
          total 0]
@@ -19,5 +19,21 @@
         (recur (rest xs) (first xs) (+ total (first xs)))
         (recur (rest xs) (first xs) total)))))
 
-(compute input)
+(part-1 input)
 ;; => 1102
+
+(defn part-2 [input]
+  (let [half-length (/ (count input) 2)]
+    (loop [xs-left (take half-length input)
+           xs-right (drop half-length input)
+           total 0]
+      (if (empty? xs-left)
+        total
+        (if (= (first xs-left) (first xs-right))
+          ;; note the * 2 in the total as we add each number which has
+          ;; the same number half way around the list twice.
+          (recur (rest xs-left) (rest xs-right) (+ total (* 2 (first xs-left))))
+          (recur (rest xs-left) (rest xs-right) total))))))
+
+(part-2 input)
+;; => 1076
